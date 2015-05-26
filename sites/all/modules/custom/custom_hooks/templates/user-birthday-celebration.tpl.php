b06f964e326669e9d9ffe0bcc3d55fd3e346e0c1
Pooja Bansal <pbansal@io-media.com>
2015-05-26 11:25:16 +0530

    changed the name from cowboys to sacramento

--- sites/all/modules/custom/custom_hooks/templates/user-birthday-celebration.tpl.php
+++ sites/all/modules/custom/custom_hooks/templates/user-birthday-celebration.tpl.php
@@ -1,6 +1,6 @@
 <div class="birthday-celebration-wrapper">
     <h1>Happy Birthday,</h1>
     <h1><?php print $name;?>!</h1>
-    <p class="form-dallas">From the Dallas Cowboys</p>
+    <p class="form-dallas">From the Sacramento Kings</p>
     <div class="form-actions form-wrapper form-group" id="edit-actions"><a href="/" class="btn btn-default form-submit">Continue to Homepage</a>
 </div>
