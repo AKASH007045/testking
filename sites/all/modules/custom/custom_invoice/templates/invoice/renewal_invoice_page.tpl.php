ea421e67ecee33ef707e685c82f743595c8ca11b
adeb <adeb@io-media.com>
2015-05-27 07:19:33 -0400

    design changes

--- sites/all/modules/custom/custom_invoice/templates/invoice/renewal_invoice_page.tpl.php
+++ sites/all/modules/custom/custom_invoice/templates/invoice/renewal_invoice_page.tpl.php
@@ -4,25 +4,25 @@
   <section class="clearfix amountdueinfo">
       <div class="col-md-7 col-xs-7">
 	  <div>Total Ticket Amount:</div>
-	  <div>Parking Amount:</div>
-	  <div>Handling Fee:</div>
+	<!--  <div>Parking Amount:</div> -->
+	<!--  <div>Handling Fee:</div> -->
 	  <div>Less Payments/Credits (-):</div>
       </div>
       <div class="contactInfoRight col-md-5 col-xs-5">                            
 	  <div><?php print isset($total) ? $total : '$0';?></div>
-	  <div><?php print isset($parking_fee) ? $parking_fee : '$0';?></div>
-	  <div><?php print isset($handling_fee) ? $handling_fee : '$0';?></div>
+	 <!-- <div><?php print isset($parking_fee) ? $parking_fee : '$0';?></div> -->
+	<!--  <div><?php print isset($handling_fee) ? $handling_fee : '$0';?></div> -->
 	  <div><?php echo $paid;?></div>
       </div>
    </section>
    <section class="clearfix amountduettl">
       <div class="col-md-7 col-xs-7">
 	  <h2>AMOUNT DUE</h2>
-	  <?php if ($payment_flag):?>
+	 <!-- <?php if ($payment_flag):?>
 	    <div class="amount-disclaimer"> Payments recently made to this account may not be reflected in the Amount Due displayed.</div>
-	  <?php endif;?>
-	  <div class="deadlineTxt">Deadline for Payment: 5/1/14</div>
-      </div>  
+	  <?php endif;?> -->
+	<!--  <div class="deadlineTxt">Deadline for Payment: 5/1/14</div> -->
+      </div>   
       <div class="amountInfoRight col-md-5 col-xs-5">
 	    <h2><?php echo $due_amount;?></h2>
       </div>
