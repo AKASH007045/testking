b310b6f56e8bf4224f7c6f1fed1cb62a58cb406c
Ravi Kumar <rkumar@io-media.com>
2017-03-30 17:59:34 +0530

    custom typeform

---
+++ modules/custom_typeform/templates/typeform_error_page.tpl.php
@@ -0,0 +1,6 @@
+<article class="amountduewrap invoice-not-awl node">
+  <section class="clearfix amountduettl">
+    <h2 class="text-center" style="color:#fff;">You have already submitted this form.</h2>
+	<p class="text-center"><a href="/home" class="btn btn-default global-button">Go to Homepage</a></p>
+  </section>
+</article>
