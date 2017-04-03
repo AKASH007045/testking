5a12b027605d0c8d04a1f341e0908349a6158922
Ravi Kumar <rkumar@io-media.com>
2017-04-03 16:28:52 +0530

    Invoice Payment Issue

--- themes/sports/js/custom_features.js
+++ themes/sports/js/custom_features.js
@@ -6,4 +6,17 @@ jQuery(document).ready(function(){
 	jQuery('button.global-button a').click(function(){
 		alert('anchor tag clicked');
 	});
+	
+	if(window.location.href.indexOf("payment") > -1){
+		jQuery('.page-invoice-payment .payment-amount-box input.form-checkbox').attr('checked', false);
+		if(jQuery('.make-payment-wrapper input.form-checkbox').length > 0){
+			jQuery('.make-payment-wrapper input.form-checkbox').attr('checked', false);
+		}else{
+			var isError = jQuery('.personal-info .has-error').length;
+			if(isError == 0){
+				jQuery('.make-payment-wrapper button#edit-submit').attr('disabled', false);
+			}
+		}
+	}
+	
 });
